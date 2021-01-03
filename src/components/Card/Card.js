a6accb0de8883dae9fb934a6460531f108b63ea7
Natalia <smolutka15@gmail.com>
2021-01-03 20:05:07 +0100

    Add Card and Column components

---
+++ src/components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+  }
+
+
+render() {
+  return (
+    <section className={styles.component}>
+    <div>{this.props.title}</div>
+    </section>
+  )
+}
+}
+
+export default Card;
