31de9061fa030a681b186bdb7d7bf2da5ab0bfa7
Natalia <smolutka15@gmail.com>
2021-01-14 13:42:33 +0100

    Add MainLayout component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './Header.scss';
+import {NavLink, Link} from 'react-router-dom';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { fa-atom } from '@fortawesome/free-solid-svg-icons';
+import Container from '../Container/Container.js';
+import Icon from '../Icon/Icon';
+
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className={styles.component}>
+        <Container>
+          <div className={styles.wrapper}>
+            <Link to='/' className={styles.logo}>
+              <Icon name='atom' />
+            </Link>
+              <nav>
+                <NavLink exact to='/' activeClassName='active'>Home</NavLink>
+                <NavLink exact to='/info' activeClassName='active'>Info</NavLink>
+              </nav>
+          </div>
+        </Container>
+      </header>
+    );
+  }
+}
+
+export default Header;
